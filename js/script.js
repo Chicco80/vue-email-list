c817c4efcf88bc8ee58220ca84d60caf630f8214
Chicco80 <chicco80lisa@gmail.com>
2022-05-02 15:14:44 +0200

    inizio

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const app = new Vue({
+    el : '#app',
+    data:{
+        mails:'',
+    },
+    methods:{
+            
+
+    },
+    created(){
+        for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{this.mails = resp.data.response
+            
+    
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+        }
+       
+
+    }
+});
