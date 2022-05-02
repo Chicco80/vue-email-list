bce159c9bd133a542073e325dc08e74c0f692fb2
Chicco80 <chicco80lisa@gmail.com>
2022-05-02 16:45:33 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 const app = new Vue({
     el : '#app',
     data:{
-        mails:'',
+        mails:[],
     },
     methods:{
             
 
     },
     created(){
-        for(let i = 0; i < 10; i++){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{this.mails = resp.data.response
-            
-    
-            })
-            .catch((error)=>{
-                console.log(error)
-            })
-        }
-       
+         for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((resp)=>{
+                this.mails.push(resp.data.response);
+                console.log(resp.data.response);
 
+            })
+             .catch((error)=>{
+                console.log(error)
+            });
+         }
     }
+    
 });
