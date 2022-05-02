1024d113c85a95ff197f194ccbf1a912e1d66729
Chicco80 <chicco80lisa@gmail.com>
2022-05-02 18:27:55 +0200

    Timeout

--- js/script.js
+++ js/script.js
@@ -4,21 +4,20 @@ const app = new Vue({
         mails:[],
     },
     methods:{
-            
-
+        getData(){
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                     .then((resp)=>{
+                     this.mails.push(resp.data.response);
+                     console.log(resp.data.response);
+    
+                }) .catch((error)=>{
+                    console.log(error)
+                })
+            }
+        }
     },
     created(){
-         for(let i = 0; i < 10; i++){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((resp)=>{
-                this.mails.push(resp.data.response);
-                console.log(resp.data.response);
-
-            })
-             .catch((error)=>{
-                console.log(error)
-            });
-         }
+        setTimeout(this.getData, 3000)
     }
-    
 });
